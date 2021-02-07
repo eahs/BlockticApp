1f58d9cd841ed41a1158e3a8002a0c4f7884f5e3
David <bertramfamily01@gmail.com>
2021-02-07 13:34:20 -0500

    Added a score counter which decreases every 100ms

---
+++ Source/Assets/Score.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+	double value=10000;
+	double pastValue=10000;
+	
+	public Text TextObject;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        //textObject = GetComponent.<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (value>0 && value < Convert.ToInt32(pastValue+1))
+        {
+			pastValue=value;
+			TextObject.text=string.Format("Score: {0}", Convert.ToInt32(value));
+        }
+        value -= Time.deltaTime*10;
+    }
+}
