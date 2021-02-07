b91319afc6a2828ebdb56b7fa2840a30a7c45aa3
David <bertramfamily01@gmail.com>
2021-02-07 15:34:45 -0500

    Added a button block
    It detects if the player presses space while near it

--- Source/Assets/Tilemap.cs
+++ Source/Assets/Tilemap.cs
@@ -7,7 +7,7 @@ public class Tilemap : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //setTile(zero,)
     }
 
     // Update is called once per frame
@@ -15,4 +15,61 @@ public class Tilemap : MonoBehaviour
     {
         
     }
+}/*
+public class HighlightInFront : MonoBehaviour
+{
+    public Tile highlightTile;
+    public Tilemap highlightMap;
+ 
+    private Vector3Int previous;
+ 
+    // do late so that the player has a chance to move in update if necessary
+    private void LateUpdate()
+    {
+        // get current grid location
+        Vector3Int currentCell = highlightMap.WorldToCell(transform.position);
+        // add one in a direction (you'll have to change this to match your directional control)
+        currentCell.x += 1;
+ 
+        // if the position has changed
+        if(currentCell != previous)
+        {
+            // set the new tile
+            highlightMap.SetTile(currentCell, highlightTile);
+ 
+            // erase previous
+            highlightMap.SetTile(previous, null);
+ 
+            // save the new position for next frame
+            previous = currentCell;
+        }
+    }
 }
+ [Serializable]
+ public class TileType
+ {
+     public int id;
+     public string name;
+     public double hardness;
+ 
+     public static TileType CreateFromJSON(string jsonString)
+     {
+         return JsonUtility.FromJson<TileType>(jsonString);
+     }
+ }
+ 
+ public class TileTypeLoader
+ {
+     public IEnumerable<TileType> LoadTileTypes()
+     {
+         var tileTypes = new List<TileType>();
+         var tileTypeStrings = File.ReadAllLines("C:/path/to/tile/type/file.json");
+         foreach(var tileTypeString in tileTypeStrings)
+         {
+             var tileType = TileType.CreateFromJSON(tileTypeString);
+             tileTypes.Add(tileType);
+         }
+ 
+         return tileTypes;
+     }
+ }*/
