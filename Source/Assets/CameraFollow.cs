a3eb06607ce51ce4666edccc0c7b17f7b3100304
Phillip <a.minecraft.phan@gmail.com>
2021-03-03 10:40:18 -0500

    Camera follows player

---
+++ Source/Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform targetToFollow;
+
+    void Update()
+    {
+        transform.position = new Vector3(
+            Mathf.Clamp(targetToFollow.position.x, -15f, 20f),
+            Mathf.Clamp(targetToFollow.position.y, 0f, 25f),
+            transform.position.z);
+    }
+}
