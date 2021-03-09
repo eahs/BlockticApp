7ff4359ca640b517fa15f03dd7baa72832d3dea1
David <bertramfamily01@gmail.com>
2021-03-09 11:33:02 -0500

    WIP: gravity flip event

---
+++ Source/Assets/FlipG.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipG : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+	
+	void onEnable()
+	{
+		ButtonBlock.OnClicked += flip;
+	}
+
+	void onDisable()
+	{
+		ButtonBlock.OnClicked -= flip;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void flip()
+	{
+		GetComponent<Rigidbody2D>().gravityScale *= -1;
+	}
+}
