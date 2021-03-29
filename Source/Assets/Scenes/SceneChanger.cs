64f90208ab2ddb39494cd747657fd0b3b276632c
David <bertramfamily01@gmail.com>
2021-03-28 22:24:41 -0400

    Binded level 7 and simplified SceneChanger.cs

--- Source/Assets/Scenes/SceneChanger.cs
+++ Source/Assets/Scenes/SceneChanger.cs
@@ -32,39 +32,8 @@ public class SceneChanger : MonoBehaviour
 		SceneManager.LoadScene(2);
 	}
 	
-	public void GoToLVL1 ()
+	public void GoToLVL(int num)
 	{
-		SceneManager.LoadScene(4);
-	}
-	
-	public void GoToLVL2 ()
-	{
-		SceneManager.LoadScene(5);
-	}
-	
-	public void GoToLVL3 ()
-	{
-		SceneManager.LoadScene(6);
-	}
-	
-	public void GoToLVL4 ()
-	{
-		SceneManager.LoadScene(7);
-	}
-	
-	public void GoToLVL5 ()
-	{
-		SceneManager.LoadScene(8);
-	}
-	
-	public void GoToLVL6()
-	{
-		SceneManager.LoadScene(9);
-	}
-	
-	
-	public void GoToTut ()
-	{
-		SceneManager.LoadScene(4);
+		SceneManager.LoadScene(3+num);
 	}
 }
