91b6519fb425a1a554a8ad37708781cd79656e02
blair <blairharris360@gmail.com>
2021-04-29 12:05:02 -0400

    added level locking

--- Source/Assets/goal.cs
+++ Source/Assets/goal.cs
@@ -3,25 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class goal : MonoBehaviour
 {
+    public int nextSceneLoad;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nextSceneLoad = SceneManager.GetActiveScene().buildIndex + 1;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D coll)
+    public void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.tag == "Player")
         {
-            UnityEngine.SceneManagement.SceneManager.LoadScene("Levels");
+            if (SceneManager.GetActiveScene().buildIndex == 21)
+            {
+                Debug.Log("You Completed ALL Levels");
+
+                //Show Win Screen or Somethin.
+            }
+            else
+            {
+                //Move to next level
+                SceneManager.LoadScene(nextSceneLoad);
+
+                //Setting Int for Index
+                if (nextSceneLoad > PlayerPrefs.GetInt("levelAt"))
+                {
+                    PlayerPrefs.SetInt("levelAt", nextSceneLoad);
+                }
+            }
         }
     }
 }
